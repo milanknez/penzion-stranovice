045225819c440ea7f72b64a22e6e52981728f2fb
Milan Kněz <milan.knez@abra.eu>
2026-05-09 23:43:04 +0200

    Standalone debug and version bump (v1.0.39)

--- debug_url.php
+++ debug_url.php
@@ -1,8 +1,13 @@
 <?php
 header('Content-Type: application/json');
-require_once 'includes/CMS.php';
+
+function getBasePath() {
+    $base = str_replace(['/includes/CMS.php', '/admin/save.php', '/admin/index.php', '/router.php', '/index.php', '/debug_url.php'], '', $_SERVER['SCRIPT_NAME'] ?? '');
+    return rtrim($base, '/');
+}
+
 echo json_encode([
     'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'] ?? 'N/A',
-    'getBasePath_detected' => CMS::getBasePath(),
-    'url_ustajeni_test' => CMS::url('ustajeni.php'),
+    'getBasePath_manual' => getBasePath(),
+    'test_url' => getBasePath() . '/ustajeni-koni',
 ], JSON_PRETTY_PRINT);
