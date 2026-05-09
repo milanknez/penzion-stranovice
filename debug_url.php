c8c31c7689a8a2af013d6c57a1d00b79d3f59974
Milan Kněz <milan.knez@abra.eu>
2026-05-09 23:41:19 +0200

    Detailed URL diagnostics (v1.0.38)

--- debug_url.php
+++ debug_url.php
@@ -1,9 +1,8 @@
 <?php
 header('Content-Type: application/json');
+require_once 'includes/CMS.php';
 echo json_encode([
     'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'] ?? 'N/A',
-    'REQUEST_URI' => $_SERVER['REQUEST_URI'] ?? 'N/A',
-    'PHP_SELF' => $_SERVER['PHP_SELF'] ?? 'N/A',
-    'DOCUMENT_ROOT' => $_SERVER['DOCUMENT_ROOT'] ?? 'N/A',
-    'SCRIPT_FILENAME' => $_SERVER['SCRIPT_FILENAME'] ?? 'N/A',
+    'getBasePath_detected' => CMS::getBasePath(),
+    'url_ustajeni_test' => CMS::url('ustajeni.php'),
 ], JSON_PRETTY_PRINT);
