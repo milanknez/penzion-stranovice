a407929aad06fcdf92e0ab417edd2a2f1273f97a
Milan Kněz <116789951+milanknez@users.noreply.github.com>
2026-08-12 22:42:59 +0200

    Auto-update: pages/404.php (v1.2.5)

--- admin/version.php
+++ admin/version.php
@@ -1,4 +1,4 @@
 <?php
 // Fida CMS Version
-define('APP_VERSION', '1.2.4');
-define('CMS_VERSION', '1.2.4');
+define('APP_VERSION', '1.2.5');
+define('CMS_VERSION', '1.2.5');
--- pages/404.php
+++ pages/404.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../admin/includes/CMS.php';
 CMS::getHeader();
 ?>
 <style>* { box-sizing: border-box; } body {margin: 0;}</style>
-<body class="bg-slate-950 text-slate-100 min-h-screen"><main><section class="section-padding"><div class="container text-center"><div class="mb-8"><h1 id="ijcl" class="text-6xl font-serif text-[var(--primary)] mb-4">404</h1><h2 class="section-title">Stránka nednalezena</h2></div><p id="ivgit" class="text-xl mb-10 opacity-80">
+<body class="bg-slate-950 text-slate-100 min-h-screen"><main><section class="section-padding"><div class="container text-center"><div class="mb-8"><h1 id="ijcl" class="text-6xl font-serif text-[var(--primary)] mb-4">404</h1><h2 class="section-title">Stránka nenalezena</h2></div><p id="ivgit" class="text-xl mb-10 opacity-80">
                     Jejda, tady nic není 👀<br/>
                     Stránka mohla být přesunuta nebo smazána.<br/>
                     Zkuste se vrátit zpět nebo přejít na úvod.
